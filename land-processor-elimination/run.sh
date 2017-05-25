9847df10e98832d8daae79ac97de74f58a7065ec
Michael Dunphy <mdunphy@eoas.ubc.ca>
2017-05-25 09:51:09 -0700

    Updates to the land processor elimination scripts

--- land-processor-elimination/run.sh
+++ land-processor-elimination/run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-MESHMASK=~/MEOPAR/NEMO-forcing/grid/mesh_mask_downbyone2.nc 
-TOOL=~/MEOPAR/NEMO-3.6-r6770/NEMOGCM/TOOLS/MPP_PREP/mpp_optimiz_zoom_nc.exe
+TOOL=$HOME/MEOPAR/NEMO-3.6-code/NEMOGCM/TOOLS/MPP_PREP/mpp_optimiz_zoom_nc.exe
 
-# The mpp_optimiz_zoom_nc tool expects the bathy file to have
+# Select mesh mask
+#MESHMASK=$HOME/MEOPAR/NEMO-forcing/grid/mesh_mask_downbyone2.nc
+MESHMASK=$HOME/MEOPAR/NEMO-forcing/grid/mesh_mask201702.nc
+
+# The mpp_optimiz_zoom_nc tool expects the mesh mask file to have
 # variable "Bathy_level", but the mesh mask produced by NEMO calls
 # it "mbathy". So we rename the variable for use with the tool.
 if [ -f $MESHMASK ]; then
+  rm -f mesh_mask_renamed.nc
   ncrename -h -v mbathy,Bathy_level $MESHMASK mesh_mask_renamed.nc
 else
   echo "Mesh mask not found"
@@ -21,6 +25,9 @@ else
   exit
 fi
 
-# Run script to convert processor.layout into tables
+# Run script to convert processor.layout into lookup table and preferred decomposition list
 python readpl.py
+
+# Tidying
+rm -f mesh_mask_renamed.nc processor.layout
 
